1e865313637101992ecd17dff37f8e363914380c
srividya-2007 <2400030154@kluniversity.in>
2025-07-28 12:06:20 +0530

    Success Message

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+              <html lang="en">
+        <head>
+          <title>About Us</title>
+        </head>
+        <body>
+          <h1>Welcome to Our Project</h1>
+          <section id="about">
+            <h2>About Section</h2>
+            <p>This project is focused on modern frontend web development using HTML, CSS, Git, and JavaScript within VS Code.</p>
+          </section>
+        </body>
+        </html>
+
+    </>
+  )
+}
+
+export default App
